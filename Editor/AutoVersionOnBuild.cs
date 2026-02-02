9434a257c791b623ab333ba154cdad45ec0ae85a
pumpedkk <130869255+pumpedkk@users.noreply.github.com>
2026-02-02 15:17:09 -0300

    Update AutoVersionOnBuild.cs

--- Editor/AutoVersionOnBuild.cs
+++ Editor/AutoVersionOnBuild.cs
@@ -2,23 +2,46 @@ using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using System;
+using System.IO;
 
-public class AutoVersionOnBuild : IPreprocessBuildWithReport
+public class AutoVersionOnBuild : IPreprocessBuildWithReport, IPostprocessBuildWithReport
 {
     public int callbackOrder => 0;
+
+    string version;
 
     public void OnPreprocessBuild(BuildReport report)
     {
         string projectName = GetBaseProjectName();
-        string version = DateTime.Now.ToString("yyyyMMdd_HHmm");
+        version = DateTime.Now.ToString("yyyyMMdd_HHmm");
 
         PlayerSettings.productName  = $"{version}_{projectName}";
         PlayerSettings.bundleVersion = version;
-
     }
+
     public void OnPostprocessBuild(BuildReport report)
     {
-        PlayerSettings.productName = GetBaseProjectName();
+        // pasta onde a build foi gerada
+        var buildPath = report.summary.outputPath;
+
+        // no Windows o outputPath vem como:
+        // .../MinhaPasta/MeuExe.exe
+        var buildFolder = Path.GetDirectoryName(buildPath);
+
+        var parent = Directory.GetParent(buildFolder).FullName;
+
+        string projectName = GetBaseProjectName();
+
+        string newFolder =
+            Path.Combine(parent, $"{version}_{projectName}");
+
+        if (!Directory.Exists(newFolder))
+        {
+            Directory.Move(buildFolder, newFolder);
+        }
+
+        // restaura o nome do produto
+        PlayerSettings.productName = projectName;
     }
 
     static string GetBaseProjectName()
